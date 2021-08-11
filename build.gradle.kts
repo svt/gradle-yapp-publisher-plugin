33b6ebf338deeb706b94514ff9aa07be143bf2f4
Josef Andersson <josef.andersson@svt.se>
2021-08-12 00:43:04 +0200

    fix!: snapshot workaround. java 11. bump deps
    Signed-off-by: Josef Andersson <josef.andersson@svt.se>

--- build.gradle.kts
+++ build.gradle.kts
@@ -63,7 +63,7 @@ gradlePlugin {
 */
 java {
     sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_11
 
     withSourcesJar()
     withJavadocJar()
